628c3c52c16f5e7ed060766427d1a2bd94ab894e
Muhet <muhedarius96@gmail.com>
2023-02-23 16:37:14 +0200

    creation of authentication

---
+++ login.js
@@ -0,0 +1,44 @@
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+
+  const response = await fetch('/users?username=' + username + '&password=' + password);
+
+  if (response.ok) {
+    const user = await response.json();
+    console.log('Logged in successfully:', user);
+  } else {
+    console.log('Login failed');
+  }
+});
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.post('/users', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = router.db.get('users').find({ username: username, password: password }).value();
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running on port 3000');
+});
